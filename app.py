631b8e937c9952a7f2ed0a329537de29cd6efe94
Harshitha-0085 <213148824harshitha@gmail.com>
2026-05-14 14:41:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from streamlit_option_menu import option_menu
 
 from utils.styles import load_css
 from pages.homepage import show_homepage
+from pages.gallery import show_gallery
 from pages.explorer import show_festival_explorer
 from pages.quiz import show_quiz
-from pages.gallery import show_gallery
+
 
 load_css()
 
