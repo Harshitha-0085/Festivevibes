adcd2250479038893a5ea755fe78c41e0faf0f89
Harshitha-0085 <213148824harshitha@gmail.com>
2026-05-14 14:36:09 +0530

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -1,1 +1,19 @@
+def get_festival_data():
 
+    festivals = {
+
+        "Bathukamma": {
+            "date": "September-October",
+            "description": "Flower festival of Telangana",
+            "regions": ["Telangana"]
+        },
+
+        "Sankranti": {
+            "date": "January",
+            "description": "Harvest festival of Telugu states",
+            "regions": ["AP","Telangana"]
+        }
+
+    }
+
+    return festivals
