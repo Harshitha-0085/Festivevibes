4ee3a65cbee0fc11083fcc6c9a624637e2c2c07b
Harshitha-0085 <213148824harshitha@gmail.com>
2026-05-14 14:37:38 +0530

    Implement festival explorer with Streamlit

--- pages/explorer.py
+++ pages/explorer.py
@@ -1,1 +1,24 @@
-show_festival_explorer()
+import streamlit as st
+from utils.data import get_festival_data
+
+def show_festival_explorer():
+
+    festivals = get_festival_data()
+
+    st.title("🎊 Festival Explorer")
+
+    for festival,data in festivals.items():
+
+        st.markdown(f"""
+
+        <div class='festival-card'>
+
+        <h3>{festival}</h3>
+
+        <p>{data["description"]}</p>
+
+        <p>📅 {data["date"]}</p>
+
+        </div>
+
+        """, unsafe_allow_html=True)
